a021ff794392c4df8c2cf9b34ff9fa0ec2957556
Yoni Schirris <yschirris@gmail.com>
2022-11-17 18:31:39 +0100

    add slidescore url and slidescore study id to download manifest. - save manifest as json - when starting the download, add slidescore_url and slidescore_study_id - add layer of slidescore_url and slidescore_study_id keys, might a user download multiple studies into a single directory, so as to not overwrite the initial values of `slidescore_study_id` if a second study is downloaded to the same directory.

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,4 +1,52 @@
 # coding=utf-8
 # Placeholder
-def test_test():
-    assert True
+"""
+Unit tests
+"""
+
+# pylint: disable=duplicate-code
+
+import json
+from pathlib import Path
+
+from slidescore_api.cli import append_to_manifest
+
+
+def test_append_to_manifest():
+    # Ignores similar lines
+    """
+    Tests the function of slidescore_api.cli.append_to_manifest
+    """
+    if Path("./test_append_to_manifest/download_config.json").is_file():
+        Path("./test_append_to_manifest/download_config.json").unlink()  # Remove file
+
+    append_to_manifest(
+        save_dir=Path("./test_append_to_manifest"), keys=["slidescore_url", "1234", "slidescore_url"], value="https"
+    )  # -> {"slidescore_url": {"1234": {"slidescore_url": "https}}}
+
+    append_to_manifest(
+        save_dir=Path("./test_append_to_manifest"), keys=["slidescore_url", "1234", "slidescore_id"], value=1234
+    )  # -> {"slidescore_url": {"1234": {"slidescore_url": "https, "slidescore_id": 1234}}}
+
+    append_to_manifest(
+        save_dir=Path("./test_append_to_manifest"), keys=["slidescore_url", "1234", "mapping", "pathname"], value=1234
+    )
+    # -> {"slidescore_url": {"1234": {"slidescore_url": "https, "slidescore_id": 1234,
+    # "mapping": {"pathname": 1234} }}}
+
+    expected_object = {
+        "slidescore_url": {"1234": {"slidescore_url": "https", "slidescore_id": 1234, "mapping": {"pathname": 1234}}}
+    }
+
+    with open("./test_append_to_manifest/download_config.json", "r", encoding="utf-8") as file:
+        obj = json.load(file)
+        assert (
+            obj == expected_object
+        ), f"expected object is {expected_object}, while the actual object on disk is {obj}"
+
+    Path("./test_append_to_manifest/download_config.json").unlink()  # Remove file
+    Path("./test_append_to_manifest").rmdir()  # Remove dir
+
+
+if __name__ == "__main__":
+    test_append_to_manifest()
